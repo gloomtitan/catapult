6bd9be83dbfbdce3d7806cca2fa86d94044cd3a1
gloomtitan <gloomtitan1337@gmail.com>
2025-04-12 13:59:30 -0400

    Update algo.py

--- Backend/algo.py
+++ Backend/algo.py
@@ -1,23 +1,22 @@
 # name-scla-econ-eng-cs-ma-chem
-# person1-7-2-11-9-4
+# person1-7-2-11-9-16
 
 def parse_schedule(raw):
     parts = raw.split('-')
     
     times = {
         "name" : int(parts[0]),
-        "scla" : = int(parts[1]),
-        "econ" : int{parts[2]},
+        "scla" : int(parts[1]),
+        "econ" : int(parts[2]),
         "engr" : int(parts[3]),
         "cs" : int(parts[4]),
         "ma" : int(parts[5]),
         "chem" : int(parts[6])
     }
-    return name, times
+    return times
 
 def eval_schedule(): # Gives us the score for a person's timetable
-    
-    name, times = parse_scedule(raw):
+    name, times = parse_scedule(raw)
     
     tue_thu = [times[c] for c in ["scla", "econ", "eng"] if times[c] > 0]
     mwf = [times[c] for c in ["ma", "cs", "chem"] if times[c] > 0]
@@ -27,3 +26,7 @@ def eval_schedule(): # Gives us the score for a person's timetable
     
     def score_block(classes, day_group):
         scores_local = {}
+        count = len(classes)
+        for t in classes:
+            score = 0
+            if 7 <= t <= 9:
