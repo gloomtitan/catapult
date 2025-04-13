bfb321e0f29572304a7365e6df90e660298e9c82
Ates <88388753+AtesIsf@users.noreply.github.com>
2025-04-12 22:55:49 -0400

    minor changes

--- Backend/algo.py
+++ Backend/algo.py
@@ -2,11 +2,18 @@ from Backend.Models.Student import Student
 from sort_students import sort_preference_weight
 import numpy as np
 
+def handle_late():
+    pass
+
+def handle_break():
+    pass
+
+def handle_gpa():
+    pass
+
 def algo_main(students: np.ndarray):
     for student in students:
         student: Student
-        
-    pass
 
 if __name__ == "__main__":
     students: np.ndarray = sort_preference_weight()
